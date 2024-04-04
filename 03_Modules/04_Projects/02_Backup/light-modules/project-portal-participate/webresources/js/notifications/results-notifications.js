9637d02a4116c9e3a7157c087692b1f34e60054b
CRPS <cristobits0101@gmail.com>
2024-04-04 16:50:30 +0100

    Añadiendo copia de seguirdad

---
+++ 03_Modules/04_Projects/02_Backup/light-modules/project-portal-participate/webresources/js/notifications/results-notifications.js
@@ -0,0 +1,9 @@
+// Function to alert the user when the amount of content they want to display is not available.
+function amountContentDisplay() {
+    alert('The amount of content you want to display is not available!');
+}
+
+// Function to alert the user when there is no content stored.
+function thereIsNoContent() {
+    alert('No content stored!');
+}
